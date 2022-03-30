68ee143ca96b4d9368fd7bfc16a251cc5a559d7b
andykash81 <k81@ngs.ru>
2022-03-30 22:45:45 +0300

    Bugfix2

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -2,10 +2,9 @@ const menuAll = document.querySelectorAll(".menu__link");
 
 for (const menu of menuAll) {
     menu.addEventListener('click', (event) => {
-        const menuSub = menu.nextElementSibling.classList.contains("menu_sub");
-        if (menuSub) {
-                menu.nextElementSibling.classList.toggle("menu_active");
-                event.preventDefault();
+        if (menu.nextElementSibling && menu.nextElementSibling.className.includes("menu_sub")) {
+            menu.nextElementSibling.classList.toggle("menu_active");
+            event.preventDefault();
         }
     })
 }
